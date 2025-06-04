1a3ee29aedcd5878220e690645c0d18ca5232345
UUest <uuest@protonmail.com>
2025-06-04 07:00:20 -0700

    fixed missing new line in help text

--- commands.go
+++ commands.go
@@ -34,6 +34,7 @@ func commandExit() error {
 func commandHelp() error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
+	fmt.Println("")
 	fmt.Println("help: Displays a help message")
 	fmt.Println("exit: Exit the Pokedex")
 	return nil
