5c93f8702f9f7ef7f6fc62b13b0728888a86964e
UUest <uuest@protonmail.com>
2025-06-04 05:39:34 -0700

    repl

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+
+	"github.com/UUest/pokecli/cleaninput"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		text := scanner.Text()
+		cleanText := cleaninput.CleanInput(text)
+		if len(cleanText) != 0 {
+			fmt.Printf("Your command was: %s\n", strings.ToLower(cleanText[0]))
+		} else {
+			fmt.Println("No command entered")
+			continue
+		}
+		if cleanText[0] == "exit" {
+			fmt.Println("Exiting...")
+			break
+		}
+	}
 }
