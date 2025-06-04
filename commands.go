64d32cf5e6ec9ae307e1dc3884ec5e8f580b6bde
UUest <uuest@protonmail.com>
2025-06-04 06:58:16 -0700

    well that didn't work

---
+++ commands.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commandMap = map[string]cliCommand{
+	"exit": {
+		name:        "exit",
+		description: "Exit the Pokedex",
+		callback:    commandExit,
+	},
+	"help": {
+		name:        "help",
+		description: "Display help information",
+		callback:    commandHelp,
+	},
+}
+
+func commandExit() error {
+	err := fmt.Errorf("Closing the Pokedex...Goodbye!")
+	fmt.Printf("%v\n", err)
+	os.Exit(0)
+	return err
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
